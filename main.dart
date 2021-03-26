7bcea8f08393e43d7ce4b26362567e4e7209de08
Ian Hickson <ian@hixie.ch>
2021-03-26 16:07:29 -0700

    read input, write output, algorithmic solution

--- main.dart
+++ main.dart
@@ -1,6 +1,29 @@
+import 'dart:convert';
 import 'dart:io';
 
-void main() {
-  // Sample input: (0.3, 0.3)
-  print("5.3\n0.3");
+import 'center.dart';
+
+final RegExp inputPattern = RegExp(r'^\(([0-9]+.[0-9]+), ([0-9]+.[0-9]+)\)\n$');
+
+void main() async {
+  try {
+    List<Point> points = <Point>[];
+    await for (String line in stdin.transform(utf8.decoder)) {
+      Match? inputParts = inputPattern.matchAsPrefix(line);
+      if (inputParts == null || inputParts.groupCount != 2)
+        throw FormatException('Invalid input data: "$line"');
+      double x = double.parse(inputParts.group(1)!);
+      double y = double.parse(inputParts.group(2)!);
+      points.add(Point(x, y));
+      if (points.length == 3) {
+        final Point center = findCenter(points);
+        print('${center.x}\n${center.y}');
+        points.clear();
+      }
+    }
+    if (points.isNotEmpty)
+      throw FormatException('Input did not contain a set of complete triangles.');
+  } on FormatException catch (error) {
+    stderr.write(error.toString());
+  }
 }
