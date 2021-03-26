4be6e70a7ac313feebe963f355aebff3f541face
treeplate <tree.plate.moo@gmail.com>
2021-03-26 15:29:21 -0700

    some fixes

--- moveMouse.py
+++ moveMouse.py
@@ -2,7 +2,10 @@ from pynput.mouse import Button, Controller
 mouse = Controller()
 click = False
 while True:
-  mouse.position = (float(input("X: ")), float(input("Y: ")))
+  try:
+    mouse.position = (float(input("X: ")), float(input("Y: ")))
+  except EOFError:
+    break
   if click:
     mouse.press(Button.left)
   
