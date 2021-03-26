b99dd97fe5f1d0514f96c26d179feb7eb6208079
treeplate <tree.plate.moo@gmail.com>
2021-03-26 15:17:10 -0700

    add click support

--- moveMouse.py
+++ moveMouse.py
@@ -1,5 +1,8 @@
 from pynput.mouse import Button, Controller
 mouse = Controller()
+click = False
 while True:
   mouse.position = (float(input("X: ")), float(input("Y: ")))
+  if click:
+    mouse.press(Button.left)
   
